de78420492051555bdec7f2ee3807193963fcc04
antonnprince <antonprince95@gmail.com>
2024-05-20 19:30:36 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        col1:'#898989',
+        col2:'#949191',
+      }
+    },
+  },
+  plugins: [],
+}
