72d1325939a0b11ca4630db0806f43096cc0a886
antonnprince <antonprince95@gmail.com>
2024-05-25 11:55:58 +0530

    hero section responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
     extend: {
+     
       colors:{
         col1:'#898989',
-        col2:'#949191',
+        col2:'#c2c3c3',
       }
     },
   },
